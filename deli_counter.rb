e5fa9cac002e4619fed057d00edb211bfbf15c8b
Susan Meyers <smmeyers24@gmail.com>
2018-08-15 18:12:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
+require 'pry'
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+
+  else
+    list = "The list is currently:"
+    katz_deli.each.with_index do |person, index|
+    list << "#{index +1}. #{person}"
+    puts list
+  end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Write your code here.
