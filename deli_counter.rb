5b78e584f11b6f2567f2f14b7e6cb8f06613728f
Susan Meyers <smmeyers24@gmail.com>
2019-09-28 21:32:54 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require 'pry'
+def line(deli)
+  if array(deli) == []
+    puts "The line is currently empty."
+    
+  if ! array == []
+    puts "The line is currently: 1. Logan, 2. Avi 3. Spenser"
+  end
+  end
+end
+   
+  
+  
+      
+ 
+ 
+  
+ 
+   
