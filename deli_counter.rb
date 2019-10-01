4d8b3df3550f51804309fb5c204281dfe5e1debc
Susan Meyers <smmeyers24@gmail.com>
2019-10-01 17:06:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,8 @@ def line(deli)
   if (deli) == []
     puts "The line is currently empty."
     
-   elsif (deli).each_with_index do |name, i|
+   elsif (deli).each_with_index.collect do |name, i|
      #binding.pry
-      
       puts "The line is currently: #{i + 1}. #{name}"
        
    
