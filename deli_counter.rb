907e60fc70bee9987cfb91717901bc86a1729cee
Susan Meyers <smmeyers24@gmail.com>
2019-10-02 00:25:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,9 +19,18 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
   
   
   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   # if katz_deli == []
   #   katz_deli << name
   # end
